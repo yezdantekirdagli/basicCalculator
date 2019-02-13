1685ce77f79e70f43603b3d8c2a779d189c993eb
Yezdan Tekirdağlı <yezdantekirdagli@Yezdan-MacBook-Air.local>
2019-02-13 03:22:23 +0300

    Initial Commit

---
+++ BasicCalcutor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicCalcutor
+//
+//  Created by Yezdan Tekirdağlı on 13.02.2019.
+//  Copyright © 2019 Yezdan Tekirdagli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
